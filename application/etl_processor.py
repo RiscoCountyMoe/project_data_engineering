805b830ef3844f7b3ea3c00cd39e94fd6b0bda16
Mirja <risiko29@gmail.com>
2025-01-31 16:50:55 +0100

    load partitions

--- application/etl_processor.py
+++ application/etl_processor.py
@@ -7,12 +7,11 @@ from application.connectors.target_connector import TargetConnector
 from minio.error import S3Error
 import multiprocessing as mp
 
-
 class EtlProcessor:
     def __init__(self):
-        self.sorce_connector = SourceConnector()
-        self.sorce_connector.connect()
-        self.engine = self.sorce_connector.get_engine()
+        self.source_connector = SourceConnector()
+        self.source_connector.connect()
+        self.engine = self.source_connector.get_engine()
         self.table_name = 'public.chicago_crime'
         self.target_connector = TargetConnector()
         self.target_connector.connect()
@@ -27,39 +26,35 @@ class EtlProcessor:
             print("No database connection or table name available.")
             return None
         
-    def transform(self, df, partition_cols=None):
+    def transform(self, df):
         columns_to_drop = ['ID', 'Case Number', 'IUCR', 'Description', 'FBI Code', 'X Coordinate',
-              'Y Coordinate', 'Latitude', 'Longitude', 'Updated On', 'Location']
+                           'Y Coordinate', 'Latitude', 'Longitude', 'Updated On', 'Location']
         df = df.drop(columns_to_drop, axis=1)
-
-        if partition_cols:
-            pq.write_to_dataset(pa.Table.from_pandas(df), root_path='dataset', partition_cols=partition_cols, compression='brotli')
-        else:
-            parquet_buffer = io.BytesIO()
-            table = pa.Table.from_pandas(df)
-            pq.write_table(table, parquet_buffer, compression='brotli')
-            parquet_buffer.seek(0)
-            return parquet_buffer
-
+        parquet_buffer = io.BytesIO()
+        table = pa.Table.from_pandas(df)
+        pq.write_table(table, parquet_buffer, compression='brotli')
+        parquet_buffer.seek(0)
+        return parquet_buffer
 
     def load(self, file, object_name):
         bucket_name = 'data'
         try:
-            self.client.put_object(bucket_name, object_name, file, len(file.getvalue()), content_type='application/csv')
+            self.client.put_object(bucket_name, object_name, file, len(file.getvalue()), content_type='application/parquet')
             print(f'{object_name} successfully uploaded to {bucket_name}.')
         except S3Error as e:
-            print('An error occured: {e}')
+            print(f'An error occurred: {e}')
 
-    def process_in_chunks(self, chunk_size, partition_cols=None):
+    def process_in_chunks(self, chunk_size):
         offset = 0
+        file_index = 0
         while True:
             df = self.extract(offset, chunk_size)
             if df is None or df.empty:
                 break
-            parquet_buffer = self.transform(df, partition_cols=None)
-            if partition_cols:
-                object_name = f'dataset/{partition_cols}/{offset}.parquet'
-            else:
-                object_name = f'{offset}.parquet'
+
+            parquet_buffer = self.transform(df)
+            object_name = f'{file_index}.parq'
             self.load(parquet_buffer, object_name)
+            
             offset += chunk_size
+            file_index += 1
--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import os
 from application.etl_processor import EtlProcessor
 
 def main():
-    partition_cols = ['Year', 'District', 'Primary Type']
     chunk_size = 100000
 
     etl_processor = EtlProcessor()
-    etl_processor.process_in_chunks(chunk_size, partition_cols)
+    etl_processor.process_in_chunks(chunk_size)
 
 if __name__ == "__main__":
     main()
